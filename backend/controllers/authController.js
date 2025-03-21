b6d5b50469b3337b1b59339c39be4068fc79475f
W.A Chamith Sandeepa Thilakarathna <sandeepachamith53@gmail.com>
2025-03-21 04:57:13 -0700

    create a error handler function

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,14 +1,13 @@
 const User = require("../model/User");
 const bcrypt = require("bcryptjs");
-module.exports.signup = async (req, res) => {
+module.exports.signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = await bcrypt.hash(password, 10);
-  const newUser = new User({ username, email, password:hashedPassword });
-  try{
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
     await newUser.save();
     res.status(201).json({ message: "User created successfully" });
-  }
-  catch(err){
-    res.status(500).json({ message: "Error creating user", error: err.message });
+  } catch (err) {
+    next(err);
   }
 };
