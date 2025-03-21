a0251a0988f14a8cf7f2dfc3de4b80444a34654c
W.A Chamith Sandeepa Thilakarathna <sandeepachamith53@gmail.com>
2025-03-20 23:02:42 -0700

    connect to the database

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,21 @@
+require("dotenv").config();
+
 const express = require("express");
+const mongoose = require("mongoose");
+
+
+
+
+//  mongodb+srv://sandeepachamith53:G3GW9sSbXxewzKZt@mernrealestate.72fcg.mongodb.net/?retryWrites=true&w=majority&appName=mernRealEstate
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+  
+
 
 const app = express();
 
