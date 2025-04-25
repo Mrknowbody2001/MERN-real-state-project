d3eabfad6edffe6de78b224da67cb3fd381d743b
W.A Chamith Sandeepa Thilakarathna <sandeepachamith53@gmail.com>
2025-04-25 11:35:02 +0530

    create get listings api

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../model/User");
 const errorHandler = require("../utils/errorHandler");
+const Listing = require("../model/listingModel");
 //
 module.exports.updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.id) {
@@ -37,8 +38,22 @@ module.exports.deleteUser = async (req, res, next) => {
   try {
     await User.findByIdAndDelete(req.params.id);
     res.clearCookie("access_token");
-    res.status(200).json("User has been deleted")
+    res.status(200).json("User has been deleted");
   } catch (error) {
     next(error);
+  }
+};
+
+//! get user listings
+module.exports.getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userId: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(403, "You can delete only your account"));
   }
 };
--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const userRouter = express.Router();
-const { updateUser, deleteUser } = require("../controllers/userController");
+const { updateUser, deleteUser, getUserListings } = require("../controllers/userController");
 const { verifyToken } = require("../utils/verifyUser");
+const { get } = require("mongoose");
 
 userRouter.put("/update/:id", verifyToken, updateUser);
 userRouter.delete("/delete/:id", verifyToken, deleteUser);
+userRouter.get("/listings/:id", verifyToken, getUserListings);
 
 module.exports = userRouter;
