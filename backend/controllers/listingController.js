43da71f59fbaf5b77cd75bc57fcb65146c268ba9
W.A Chamith Sandeepa Thilakarathna <sandeepachamith53@gmail.com>
2025-04-29 09:43:16 +0530

    create a delete api

--- backend/controllers/listingController.js
+++ backend/controllers/listingController.js
@@ -8,3 +8,20 @@ module.exports.createListing = async (req, res, next) => {
     next(error);
   }
 };
+
+//! delete listing
+module.exports.deleteListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+      return next(errorHandler(404, "Listing not found"));
+    }
+    if (listing.userRef.toString() !== req.user.id) {
+      return next(errorHandler(403, "You can delete only your listing"));
+    }
+    await Listing.findByIdAndDelete(req.params.id);
+    res.status(200).json("Listing has been deleted");
+  } catch (error) {
+    next(error);
+  }
+};
--- backend/routes/listing.js
+++ backend/routes/listing.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { createListing } = require("../controllers/listingController");
+const { createListing, deleteListing } = require("../controllers/listingController");
 const { verifyToken } = require("../utils/verifyUser");
 const listingRouter = express.Router();
 
 listingRouter.post("/create",verifyToken, createListing);
+listingRouter.delete("/delete/:id",verifyToken, deleteListing);
 
 module.exports = listingRouter;
